d30acd057cca0353ca3afcc9ff647a1a83124ee7
Dakota Walsh <kota@nilsu.org>
2020-11-13 21:29:37 -0500

    add: error checking for download

--- cmd/add.go
+++ cmd/add.go
@@ -110,6 +110,9 @@ func add(mod int, version string, loader string) error {
 	selected := files[0]
 	// TODO: Make this toggle-able with a verbose flag
 	util.DebugFilePrint(selected)
-	curse.Download(selected.DownloadUrl, selected.FileName)
+	err := curse.Download(selected.DownloadUrl, selected.FileName)
+	if err != nil {
+		return err
+	}
 	return nil
 }
